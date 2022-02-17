a101bb26265da5cf4cc21943c82f8b6e514d98ef
Vinícius Müller <vinigm.nho@gmail.com>
2022-02-17 16:29:51 -0300

    Rename app

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
   {
-    description = "TODO: Add description";
+    description = "Flake for the tt program";
     inputs.flake-utils.url = "github:numtide/flake-utils";
 
     outputs = { self, nixpkgs, flake-utils }:
       flake-utils.lib.simpleFlake {
         inherit self nixpkgs;
-        name = "twenty-twenty";
+        name = "tt";
         shell = ./shell.nix;
       };
   }
